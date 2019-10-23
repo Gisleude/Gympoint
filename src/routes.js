60c2599194fa06bd724ddbb8762cfa7b39de55f5
Gisleude Linhares <gisleudenem@live.com>
2019-10-23 12:00:47 -0300

    Adicionando eslint, prettier e sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hi Newbie' })
-})
+routes.get('/', (req, res) => res.json({ message: 'Hi Newbie' }));
 
-export default routes
+export default routes;
