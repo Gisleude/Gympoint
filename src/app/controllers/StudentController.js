a626d77fe2f4b65064d6c082c90791fc6236f6dc
Gisleude Linhares <gisleudenem@live.com>
2019-10-29 10:50:37 -0300

    Cadastro e Atualização de Alunos

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,82 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.string().required(),
+      height: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already Exists' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.string(),
+      height: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, id } = req.body;
+
+    const student = await Student.findOne({ where: { id } });
+
+    const studentExists = await Student.findOne({ where: { email } });
+
+    if (studentExists) {
+      return res
+        .status(400)
+        .json({ error: 'Student email already registered' });
+    }
+
+    const { name, age, weight, height } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
